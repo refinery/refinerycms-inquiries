6d18557291b994f8f85c0bd83e8471def0b4e903
Lee Irving <irvingld@gmail.com>
2010-10-07 06:44:55 -0700

    removed .value from the InquirySetting method calls as these return a string, which does not have the value method.

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -1,17 +1,17 @@
 class InquiryMailer < ActionMailer::Base
 
   def confirmation(inquiry, request)
-    subject     InquirySetting.confirmation_subject.value
+    subject     InquirySetting.confirmation_subject
     recipients  inquiry.email
     from        "\"#{RefinerySetting[:site_name]}\" <no-reply@#{request.domain(RefinerySetting.find_or_set(:tld_length, 1))}>"
-    reply_to    InquirySetting.notification_recipients.value.split(',').first
+    reply_to    InquirySetting.notification_recipients.split(',').first
     sent_on     Time.now
     @inquiry =  inquiry
   end
 
   def notification(inquiry, request)
-    subject     InquirySetting.notification_subject.value
-    recipients  InquirySetting.notification_recipients.value
+    subject     InquirySetting.notification_subject
+    recipients  InquirySetting.notification_recipients
     from        "\"#{RefinerySetting[:site_name]}\" <no-reply@#{request.domain(RefinerySetting.find_or_set(:tld_length, 1))}>"
     sent_on     Time.now
     @inquiry =  inquiry
