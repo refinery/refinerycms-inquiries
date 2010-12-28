ec4af213f4ddc4aba8f03e2907628c7e1c7727dc
Philip Arndt <parndt@gmail.com>
2010-12-29 00:25:17 +1300

    Re-use index action for spam, too. Added pagination!

--- app/controllers/admin/inquiries_controller.rb
+++ app/controllers/admin/inquiries_controller.rb
@@ -1,19 +1,20 @@
 class Admin::InquiriesController < Admin::BaseController
 
   crudify :inquiry, :title_attribute => "name", :order => "created_at DESC"
+  helper_method :group_by_date
 
+  before_filter :find_all_ham, :only => [:ham]
+  before_filter :find_all_spam, :only => [:spam]
   before_filter :get_spam_count, :only => [:index, :spam]
 
   def index
-    @inquiries = Inquiry.ham.with_query(params[:search]) if searching?
-
-    @grouped_inquiries = group_by_date(Inquiry.ham)
+    @inquiries = @inquiries.with_query(params[:search]) if searching?
+    @inquiries = @inquiries.paginate({:page => params[:page]})
   end
 
   def spam
-    @inquiries = Inquiry.spam.with_query(params[:search]) if searching?
-
-    @grouped_inquiries = group_by_date(Inquiry.spam)
+    self.index
+    render :action => 'index'
   end
 
   def toggle_spam
@@ -24,6 +25,14 @@ class Admin::InquiriesController < Admin::BaseController
   end
 
 protected
+
+  def find_all_ham
+    @inquiries = Inquiry.ham
+  end
+
+  def find_all_spam
+    @inquiries = Inquiry.spam
+  end
 
   def get_spam_count
     @spam_count = Inquiry.count(:conditions => {:spam => true})
