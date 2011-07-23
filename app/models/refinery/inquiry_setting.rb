6a50b4673d2940ab612b4ef5ee3b012acfcdb805
Uģis Ozols <ugis.ozolss@gmail.com>
2011-07-24 00:21:44 +0300

    Add scoping to inquiry settings.

--- app/models/refinery/inquiry_setting.rb
+++ app/models/refinery/inquiry_setting.rb
@@ -4,39 +4,49 @@ module Refinery
     class << self
       def confirmation_subject(locale='en')
         Refinery::Setting.find_or_set("inquiry_confirmation_subject_#{locale}".to_sym,
-                                      "Thank you for your inquiry")
+                                      "Thank you for your inquiry",
+                                      :scoping => "inquiries")
       end
 
       def confirmation_subject=(value)
         value.first.keys.each do |locale|
-          Refinery::Setting.set("inquiry_confirmation_subject_#{locale}".to_sym, value.first[locale.to_sym])
+          Refinery::Setting.set("inquiry_confirmation_subject_#{locale}".to_sym,
+                                value.first[locale.to_sym],
+                                :scoping => "inquiries")
         end
       end
 
       def confirmation_message(locale='en')
         Refinery::Setting.find_or_set("inquiry_confirmation_messeage_#{locale}".to_sym,
                                       "Thank you for your inquiry %name%,\n\nThis email is a receipt to confirm 
-                                      we have received your inquiry and we'll be in touch shortly.\n\nThanks.")
+                                      we have received your inquiry and we'll be in touch shortly.\n\nThanks.",
+                                      :scoping => "inquiries")
       end
 
       def confirmation_message=(value)
         value.first.keys.each do |locale|
-          Refinery::Setting.set("inquiry_confirmation_messeage_#{locale}".to_sym, value.first[locale.to_sym])
+          Refinery::Setting.set("inquiry_confirmation_messeage_#{locale}".to_sym,
+                                value.first[locale.to_sym],
+                                :scoping => "inquiries")
         end
       end
 
       def notification_recipients
         Refinery::Setting.find_or_set(:inquiry_notification_recipients,
-                                      ((Refinery::Role[:refinery].users.first.email rescue nil) if defined?(Refinery::Role)).to_s)
+                                      ((Refinery::Role[:refinery].users.first.email rescue nil) if defined?(Refinery::Role)).to_s,
+                                      :scoping => "inquiries")
       end
 
       def notification_subject
         Refinery::Setting.find_or_set(:inquiry_notification_subject,
-                                      "New inquiry from your website")
+                                      "New inquiry from your website",
+                                      :scoping => "inquiries")
       end
       
       def send_confirmation?
-        Refinery::Setting.find_or_set(:inquiry_send_confirmation, true)
+        Refinery::Setting.find_or_set(:inquiry_send_confirmation, 
+                                      true,
+                                      :scoping => "inquiries")
       end
     end
 
