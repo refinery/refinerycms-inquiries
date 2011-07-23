f49140228a336a8d6fe49a952f1123305c5800d4
Uģis Ozols <ugis.ozolss@gmail.com>
2011-07-23 16:52:34 +0300

    Fix up deprecated stuff in mailer.

--- app/mailers/refinery/inquiry_mailer.rb
+++ app/mailers/refinery/inquiry_mailer.rb
@@ -2,20 +2,18 @@ module Refinery
   class InquiryMailer < ActionMailer::Base
 
     def confirmation(inquiry, request)
-      subject     Refinery::InquirySetting.confirmation_subject(Globalize.locale)
-      recipients  inquiry.email
-      from        "\"#{Refinery::Setting[:site_name]}\" <no-reply@#{request.domain(Refinery::Setting.find_or_set(:tld_length, 1))}>"
-      reply_to    Refinery::InquirySetting.notification_recipients.split(',').first
-      sent_on     Time.now
       @inquiry =  inquiry
+      mail :subject   => Refinery::InquirySetting.confirmation_subject(Globalize.locale),
+           :to        => inquiry.email,
+           :from      => "\"#{Refinery::Setting[:site_name]}\" <no-reply@#{request.domain(Refinery::Setting.find_or_set(:tld_length, 1))}>",
+           :reply_to  => Refinery::InquirySetting.notification_recipients.split(',').first
     end
 
     def notification(inquiry, request)
-      subject     Refinery::InquirySetting.notification_subject
-      recipients  Refinery::InquirySetting.notification_recipients
-      from        "\"#{Refinery::Setting[:site_name]}\" <no-reply@#{request.domain(Refinery::Setting.find_or_set(:tld_length, 1))}>"
-      sent_on     Time.now
       @inquiry =  inquiry
+      mail :subject => Refinery::InquirySetting.notification_subject,
+           :to      => Refinery::InquirySetting.notification_recipients,
+           :from    => "\"#{Refinery::Setting[:site_name]}\" <no-reply@#{request.domain(Refinery::Setting.find_or_set(:tld_length, 1))}>"
     end
 
   end
