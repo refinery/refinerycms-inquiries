3bb516a998f2864edbd1a488e8e0689eae0a8267
Uģis Ozols <ugis.ozolss@gmail.com>
2011-08-05 08:01:38 +0300

    Replace create inquiry cucumber feature with rspec request specs.

--- spec/requests/create_inquiries_spec.rb
+++ spec/requests/create_inquiries_spec.rb
@@ -8,21 +8,42 @@ describe "create inquiry" do
     load File.expand_path("../../../db/seeds/pages_for_inquiries.rb", __FILE__)
   end
 
-  it "is successful" do
-    visit new_inquiry_path
+  context "when valid data" do
+    it "is successful" do
+      visit new_inquiry_path
 
-    fill_in "Name", :with => "Ugis Ozols"
-    fill_in "Email", :with => "ugis.ozols@refinerycms.com"
-    fill_in "Message", :with => "Hey, I'm testing!"
-    click_button "Send message"
+      fill_in "Name", :with => "Ugis Ozols"
+      fill_in "Email", :with => "ugis.ozols@refinerycms.com"
+      fill_in "Message", :with => "Hey, I'm testing!"
+      click_button "Send message"
 
-    page.current_path.should == thank_you_inquiries_path
-    page.should have_content("Thank You")
+      page.current_path.should == thank_you_inquiries_path
+      page.should have_content("Thank You")
 
-    within "#body_content_left" do
-      page.should have_content("We've received your inquiry and will get back to you with a response shortly.")
-      page.should have_content("Return to the home page")
-      page.should have_selector("a[href='/']")
+      within "#body_content_left" do
+        page.should have_content("We've received your inquiry and will get back to you with a response shortly.")
+        page.should have_content("Return to the home page")
+        page.should have_selector("a[href='/']")
+      end
+
+      Refinery::Inquiry.count.should == 1
+    end
+  end
+
+  context "when invalid data" do
+    it "is not successful" do
+      visit new_inquiry_path
+
+      click_button "Send message"
+
+      page.current_path.should == new_inquiry_path
+      page.should have_content("There were problems with the following fields")
+      page.should have_content("Name can't be blank")
+      page.should have_content("Email is invalid")
+      page.should have_content("Message can't be blank")
+      page.should have_no_content("Phone can't be blank")
+
+      Refinery::Inquiry.count.should == 0
     end
   end
 
