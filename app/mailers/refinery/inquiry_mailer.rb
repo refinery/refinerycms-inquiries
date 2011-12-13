449a8c59f1007b744c30e8bb1209c18d5a7977cc
Uģis Ozols <ugis.ozolss@gmail.com>
2011-12-13 09:51:19 +0200

    Remove Refinery's tld_length setting. See 7ee6f9d163

--- app/mailers/refinery/inquiry_mailer.rb
+++ app/mailers/refinery/inquiry_mailer.rb
@@ -2,18 +2,18 @@ module Refinery
   class InquiryMailer < ActionMailer::Base
 
     def confirmation(inquiry, request)
-      @inquiry =  inquiry
+      @inquiry = inquiry
       mail :subject   => Refinery::InquirySetting.confirmation_subject(Globalize.locale),
            :to        => inquiry.email,
-           :from      => "\"#{Refinery::Setting[:site_name]}\" <no-reply@#{request.domain(Refinery::Setting.find_or_set(:tld_length, 1))}>",
+           :from      => "\"#{Refinery::Setting[:site_name]}\" <no-reply@#{request.domain}>",
            :reply_to  => Refinery::InquirySetting.notification_recipients.split(',').first
     end
 
     def notification(inquiry, request)
-      @inquiry =  inquiry
+      @inquiry = inquiry
       mail :subject => Refinery::InquirySetting.notification_subject,
            :to      => Refinery::InquirySetting.notification_recipients,
-           :from    => "\"#{Refinery::Setting[:site_name]}\" <no-reply@#{request.domain(Refinery::Setting.find_or_set(:tld_length, 1))}>"
+           :from    => "\"#{Refinery::Setting[:site_name]}\" <no-reply@#{request.domain}>"
     end
 
   end
