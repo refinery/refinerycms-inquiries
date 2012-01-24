6ac7866e03650f9503e5f494abd845a72e1340c4
Philip Arndt <parndt@gmail.com>
2012-01-24 17:17:20 +1300

    Use absolute controller

--- lib/refinery/inquiries/engine.rb
+++ lib/refinery/inquiries/engine.rb
@@ -12,7 +12,7 @@ module Refinery
         Refinery::Plugin.register do |plugin|
           plugin.pathname = root
           plugin.name = "refinery_inquiries"
-          plugin.url = {:controller => 'refinery/admin/inquiries'}
+          plugin.url = {:controller => '/refinery/admin/inquiries'}
           plugin.menu_match = %r{refinery/inquir(ies|y_settings)(/.+?)?$}
           plugin.activity = {
             :class_name => :'refinery/inquiry',
