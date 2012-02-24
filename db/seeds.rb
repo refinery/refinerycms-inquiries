4ab3b57dc35172d95f9b6bb57e45ecb8ec4be8c2
Uģis Ozols <ugis.ozolss@gmail.com>
2012-02-24 20:47:44 +0200

    Remove position from seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,25 @@
 ::Refinery::User.all.each do |user|
   if user.plugins.where(:name => 'refinery_inquiries').blank?
-    user.plugins.create(:name => "refinery_inquiries",
-                        :position => (user.plugins.maximum(:position) || -1) +1)
+    user.plugins.create(:name => "refinery_inquiries")
   end
 end if defined?(::Refinery::User)
 
 if defined?(::Refinery::Page)
-  page_position = (::Refinery::Page.maximum(:position, :conditions => {:parent_id => nil}) || -1)
-
   unless Refinery::Page.where(:link_url => '/contact').any?
     contact_us_page = ::Refinery::Page.create({
       :title => "Contact",
       :link_url => "/contact",
       :menu_match => "^/(inquiries|contact).*$",
-      :deletable => false,
-      :position => (page_position += 1)
+      :deletable => false
     })
     contact_us_page.parts.create({
       :title => "Body",
-      :body => "<p>Get in touch with us. Just use the form below and we'll get back to you as soon as we can.</p>",
-      :position => 0
+      :body => "<p>Get in touch with us. Just use the form below and we'll get back to you as soon as we can.</p>"
     })
     contact_us_page.parts.create({
       :title => "Side Body",
-      :body => "",
-      :position => 1
+      :body => ""
     })
-    contact_us_page_position = -1
   end
 
   unless Refinery::Page.where(:link_url => '/contact/thank_you').any?
@@ -35,13 +28,11 @@ if defined?(::Refinery::Page)
       :link_url => "/contact/thank_you",
       :menu_match => "^/(inquiries|contact)/thank_you$",
       :show_in_menu => false,
-      :deletable => false,
-      :position => (contact_us_page_position += 1)
+      :deletable => false
     })
     thank_you_page.parts.create({
       :title => "Body",
-      :body => "<p>We've received your inquiry and will get back to you with a response shortly.</p><p><a href='/'>Return to the home page</a></p>",
-      :position => 0
+      :body => "<p>We've received your inquiry and will get back to you with a response shortly.</p><p><a href='/'>Return to the home page</a></p>"
     })
   end
 
@@ -49,13 +40,11 @@ if defined?(::Refinery::Page)
     privacy_policy_page = contact_us_page.children.create({
       :title => "Privacy Policy",
       :deletable => true,
-      :show_in_menu => false,
-      :position => (contact_us_page_position += 1)
+      :show_in_menu => false
     })
     privacy_policy_page.parts.create({
       :title => "Body",
-      :body => "<p>We respect your privacy. We do not market, rent or sell our email list to any outside parties.</p><p>We need your e-mail address so that we can ensure that the people using our forms are bona fide. It also allows us to send you e-mail newsletters and other communications, if you opt-in. Your postal address is required in order to send you information and pricing, if you request it.</p><p>Please call us at 123 456 7890 if you have any questions or concerns.</p>",
-      :position => 0
+      :body => "<p>We respect your privacy. We do not market, rent or sell our email list to any outside parties.</p><p>We need your e-mail address so that we can ensure that the people using our forms are bona fide. It also allows us to send you e-mail newsletters and other communications, if you opt-in. Your postal address is required in order to send you information and pricing, if you request it.</p><p>Please call us at 123 456 7890 if you have any questions or concerns.</p>"
     })
   end
 end
