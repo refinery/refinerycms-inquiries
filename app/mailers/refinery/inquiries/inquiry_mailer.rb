ae8b8517e3ac891263f891e41a1197f21e9c35d6
Gustavo Soares <gustavosoares@gmail.com>
2012-09-04 23:49:40 -0300

    adds the ability set the from email

--- app/mailers/refinery/inquiries/inquiry_mailer.rb
+++ app/mailers/refinery/inquiries/inquiry_mailer.rb
@@ -6,7 +6,7 @@ module Refinery
         @inquiry = inquiry
         mail :subject   => Refinery::Inquiries::Setting.confirmation_subject(Globalize.locale),
              :to        => inquiry.email,
-             :from      => "\"#{Refinery::Core.site_name}\" <no-reply@#{request.domain}>",
+             :from      => "\"#{Refinery::Core.site_name}\" <#{Refinery::Inquiries.inquiry_from_email}>",
              :reply_to  => Refinery::Inquiries::Setting.notification_recipients.split(',').first
       end
 
@@ -14,7 +14,7 @@ module Refinery
         @inquiry = inquiry
         mail :subject => Refinery::Inquiries::Setting.notification_subject,
              :to      => Refinery::Inquiries::Setting.notification_recipients,
-             :from    => "\"#{Refinery::Core.site_name}\" <no-reply@#{request.domain}>"
+             :from      => "\"#{Refinery::Core.site_name}\" <#{Refinery::Inquiries.inquiry_from_email}>",
       end
 
     end
--- lib/refinery/inquiries/configuration.rb
+++ lib/refinery/inquiries/configuration.rb
@@ -2,8 +2,10 @@ module Refinery
   module Inquiries
     include ActiveSupport::Configurable
 
-    config_accessor :show_contact_privacy_link
+    config_accessor :show_contact_privacy_link, :inquiry_from_email
 
     self.show_contact_privacy_link = true
+    self.inquiry_from_email        = "no-reply@localhost"
+
   end
 end
