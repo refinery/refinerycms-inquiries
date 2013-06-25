e263dfb03df251467be4c180dcdeabea598c5ec4
Ole Reifschneider <mail@ole-reifschneider.de>
2013-06-25 17:18:19 +0200

    Add option to send notifications for spam messages
    Conflicts:
    	lib/generators/refinery/inquiries/templates/config/initializers/refinery/inquiries.rb.erb
    	lib/refinery/inquiries/configuration.rb

--- lib/refinery/inquiries/configuration.rb
+++ lib/refinery/inquiries/configuration.rb
@@ -3,7 +3,9 @@ module Refinery
     include ActiveSupport::Configurable
 
     config_accessor :show_contact_privacy_link
+    config_accessor :send_notifications_for_inquiries_marked_as_spam
 
     self.show_contact_privacy_link = true
+    self.send_notifications_for_inquiries_marked_as_spam = false
   end
 end
