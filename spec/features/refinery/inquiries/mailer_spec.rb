d0928e445a535658933eddedeeddf96db05abf2c
martin91 <hongzeqin@gmail.com>
2013-09-23 13:13:21 +0800

    Modify the test example for mailer

--- spec/features/refinery/inquiries/mailer_spec.rb
+++ spec/features/refinery/inquiries/mailer_spec.rb
@@ -22,7 +22,7 @@ module Refinery
       it "sends confirmation email" do
         open_email("ugis.ozols@refinerycms.com")
 
-        current_email.from.should eq("Ugis Ozols [#{Refinery::Core.site_name}] <#{Refinery::Inquiries.from_name}@example.com>")
+        current_email.from.should eq("<#{Refinery::Inquiries.from_name}@example.com>")
         current_email.to.should eq(["ugis.ozols@refinerycms.com"])
         current_email.subject.should eq("Thank you for your inquiry")
         current_email.body.should eq("Thank you for your inquiry Ugis Ozols,\n\nThis email is a receipt to confirm we have received your inquiry and we'll be in touch shortly.\n\nThanks.")
@@ -31,7 +31,7 @@ module Refinery
       it "sends notification email" do
         open_email("rspec@refinerycms.com")
 
-        current_email.from.should eq("Ugis Ozols [#{Refinery::Core.site_name}] <#{Refinery::Inquiries.from_name}@example.com>")
+        current_email.from.should eq("<#{Refinery::Inquiries.from_name}@example.com>")
         current_email.to.should eq(["rspec@refinerycms.com"])
         current_email.subject.should eq("New inquiry from your website")
         current_email.body.should eq("Hi there,\n\nYou just received a new inquiry on your website.\n\n--- inquiry starts ---\n\nFrom: Ugis Ozols\nEmail: ugis.ozols@refinerycms.com\nPhone: \nMessage:\nHey, I'm testing!\n\n--- inquiry ends ---\n\nKind Regards,\nCompany Name\n\nP.S. All your inquiries are stored in the \"Inquiries\" section of Refinery should you ever want to view it later there.")
