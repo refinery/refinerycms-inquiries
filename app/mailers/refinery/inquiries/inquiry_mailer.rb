c514d9a33abe98b18d8c772f93749b7eb90e65ae
martin91 <hongzeqin@gmail.com>
2013-09-23 12:12:11 +0800

    Fix error :from option value for inquiry mailer
    Because the SMTP limits, it is not allowed to prepend any host name
    before the mailbox of the sender.

--- app/mailers/refinery/inquiries/inquiry_mailer.rb
+++ app/mailers/refinery/inquiries/inquiry_mailer.rb
@@ -6,10 +6,7 @@ module Refinery
         @inquiry = inquiry
         mail :subject   => Refinery::Inquiries::Setting.confirmation_subject(Globalize.locale),
              :to        => inquiry.email,
-             :from      => ::I18n.t('from_name',
-                                    :scope => 'refinery.inquiries.config',
-                                    :site_name => Refinery::Core.site_name,
-                                    :name => @inquiry.name) + " <#{Refinery::Inquiries.from_name}@#{request.domain}>",
+             :from      => "<#{Refinery::Inquiries.from_name}@#{request.domain}>",
              :reply_to  => Refinery::Inquiries::Setting.notification_recipients.split(',').first
       end
 
@@ -17,10 +14,7 @@ module Refinery
         @inquiry = inquiry
         mail :subject   => Refinery::Inquiries::Setting.notification_subject,
              :to        => Refinery::Inquiries::Setting.notification_recipients,
-             :from      => ::I18n.t('from_name',
-                                    :scope => 'refinery.inquiries.config',
-                                    :site_name => Refinery::Core.site_name,
-                                    :name => @inquiry.name) + " <#{Refinery::Inquiries.from_name}@#{request.domain}>",
+             :from      => "<#{Refinery::Inquiries.from_name}@#{request.domain}>",
              :reply_to  => inquiry.email
       end
 
