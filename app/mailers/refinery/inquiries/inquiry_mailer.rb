adb765882776be5b2bb556bab8a8e48796133234
duccioarmenise <duccioarmenise@gmail.com>
2013-10-11 11:38:42 +0200

    inquriy_mailer.rb refactored in order to remove duplication

--- app/mailers/refinery/inquiries/inquiry_mailer.rb
+++ app/mailers/refinery/inquiries/inquiry_mailer.rb
@@ -6,10 +6,7 @@ module Refinery
         @inquiry = inquiry
         mail :subject   => Refinery::Inquiries::Setting.confirmation_subject(Globalize.locale),
              :to        => inquiry.email,
-             :from      => ::I18n.t('from_name',
-                                    :scope => 'refinery.inquiries.config',
-                                    :site_name => Refinery::Core.site_name,
-                                    :name => @inquiry.name) + " <#{Refinery::Inquiries.from_name}@#{request.domain}>",
+             :from      => from_field_for(@inquiry, request),
              :reply_to  => Refinery::Inquiries::Setting.notification_recipients.split(',').first
       end
 
@@ -17,13 +14,19 @@ module Refinery
         @inquiry = inquiry
         mail :subject   => Refinery::Inquiries::Setting.notification_subject,
              :to        => Refinery::Inquiries::Setting.notification_recipients,
-             :from      => ::I18n.t('from_name',
-                                    :scope => 'refinery.inquiries.config',
-                                    :site_name => Refinery::Core.site_name,
-                                    :name => @inquiry.name) + " <#{Refinery::Inquiries.from_name}@#{request.domain}>",
+             :from      => from_field_for(@inquiry, request),
              :reply_to  => inquiry.email
       end
-
+      
+      private
+      
+      def from_field_for(inquiry, request)
+        ::I18n.t('from_name',
+                 :scope => 'refinery.inquiries.config',
+                 :site_name => Refinery::Core.site_name,
+                 :name => inquiry.name) + " <#{Refinery::Inquiries.from_name}@#{request.domain}>"
+      end
+      
     end
   end
 end
