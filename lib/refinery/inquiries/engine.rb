c0af1f31a15bec043c5efeb522a224cfc7446c1e
Philip Arndt <p@arndt.io>
2014-10-15 19:22:11 +0100

    Removed call to deprecated API plugin#activity=

--- lib/refinery/inquiries/engine.rb
+++ lib/refinery/inquiries/engine.rb
@@ -11,12 +11,6 @@ module Refinery
           plugin.name = "refinerycms_inquiries"
           plugin.url = proc { Refinery::Core::Engine.routes.url_helpers.inquiries_admin_inquiries_path }
           plugin.menu_match = %r{refinery/inquiries(/.+?)?$}
-          plugin.activity = {
-            :class_name => :'refinery/inquiries/inquiry',
-            :title => 'name',
-            :url_prefix => nil,
-            :url => 'refinery.inquiries_admin_inquiries_path'
-          }
         end
       end
 
