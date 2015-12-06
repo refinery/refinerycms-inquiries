f083a8e6700a47cde687f5efb91f82c177364ef4
Brice Sanchez <bricesanchez@users.noreply.github.com>
2015-12-05 23:07:59 -0500

    Add permitted_inquiry_params method

--- app/controllers/refinery/inquiries/inquiries_controller.rb
+++ app/controllers/refinery/inquiries/inquiries_controller.rb
@@ -2,8 +2,8 @@ module Refinery
   module Inquiries
     class InquiriesController < ::ApplicationController
 
-      before_filter :find_page, only: [:create, :new]
-      before_filter :find_thank_you_page, only: :thank_you
+      before_action :find_page, only: [:create, :new]
+      before_action :find_thank_you_page, only: :thank_you
 
       def thank_you
       end
@@ -34,7 +34,7 @@ module Refinery
 
           redirect_to refinery.thank_you_inquiries_inquiries_path
         else
-          render :action => 'new'
+          render action: 'new'
         end
       end
 
@@ -48,10 +48,14 @@ module Refinery
         @page = Page.find_by(link_url: Refinery::Inquiries.page_path_thank_you)
       end
 
-      private
-
       def inquiry_params
-        params.require(:inquiry).permit(:name, :phone, :message, :email)
+        params.require(:inquiry).permit(permitted_inquiry_params)
+      end
+      
+      private
+      
+      def permitted_inquiry_params
+        [:name, :phone, :message, :email]
       end
 
     end
