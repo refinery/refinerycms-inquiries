e3e9163522252b22662559cb2a637a1b0a753c77
Marc <brigriffin@users.noreply.github.com>
2018-06-13 23:33:51 +0200

    Add optional company field in inquiry form

--- lib/refinery/inquiries/configuration.rb
+++ lib/refinery/inquiries/configuration.rb
@@ -3,6 +3,7 @@ module Refinery
     include ActiveSupport::Configurable
 
     config_accessor :show_contact_privacy_link
+    config_accessor :show_company_field
     config_accessor :show_phone_number_field
     config_accessor :show_placeholders
     config_accessor :send_notifications_for_inquiries_marked_as_spam
@@ -11,6 +12,7 @@ module Refinery
     config_accessor :filter_spam, :recaptcha_site_key
 
     self.show_contact_privacy_link = true
+    self.show_company_field = false
     self.show_phone_number_field = true
     self.show_placeholders = true
     self.send_notifications_for_inquiries_marked_as_spam = false
